8da85e788e6556d0612f4e7fedaa51503f7cfeaa
muction <muction@yeah.net>
2020-01-20 17:56:37 +0800

    dev

---
+++ Src/Lib/Patch/AppPatch.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Stars\Tools\Lib\Patch;
+
+/**
+ * App 打补丁工具
+ * Interface AppPatch
+ * @package Stars\Tools\Lib\Patch
+ */
+interface AppPatch
+{
+    public function patch();
+
+
+    public function update();
+
+
+}
